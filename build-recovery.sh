27baf92fcf0514a62ba4fc3dc74bcff355a329aa
youyim123 <youyim@hotmail.com>
2017-11-16 14:50:14 +0800

    Update build-recovery.sh

--- build-recovery.sh
+++ build-recovery.sh
@@ -11,6 +11,5 @@ echo -e "\033[01;32mlunch cm_nx531j-eng[0m"
 lunch cm_nx531j-eng
 echo -e "\033[01;32mmka recovery.img[0m"
 mka recoveryimage
-echo -e "\033[01;32mrecovery_signer boot.img...\033[0m"
-java -jar device/nubia/nx531j/bootsignature/BootSignature.jar /recovery out/target/product/nx531j/recovery.img device/nubia/nx531j/bootsignature/verity.pk8 device/nubia/nx531j/bootsignature/verity.x509.der out/target/product/nx531j/TWRP_multirom_$(TARGET_DEVICE)_`cat $(MR_REC_BUILD_NUMBER_FILE)`.img.img
+
 
