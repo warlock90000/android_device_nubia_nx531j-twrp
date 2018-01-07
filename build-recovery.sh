96809d2a96650f12156399f319b4b6d1ad90f1c8
youyim123 <youyim@hotmail.com>
2018-01-07 12:18:13 +0800

    Update build-recovery.sh

--- build-recovery.sh
+++ build-recovery.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo -e "\033[01;32mGo to the command execution directory\033[0m "
-cd ..
-cd ..
-cd ..
+cd ~/lineage
 echo -e "\033[01;32mmake clean\033[0m"
 make clean
 echo -e "\033[01;32msource build/envsetup.sh[0m"
